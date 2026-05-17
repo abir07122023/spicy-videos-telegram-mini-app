5c57182521ec057a35fa276bdd5a4cf4e1ce32a0
abir07122023 <mostakabir2007@gmail.com>
2026-05-17 14:38:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,29 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 function watchAd() {
+const tg = window.Telegram.WebApp;
 
-  alert("Fake ad playing...");
+tg.expand();
+
+function watchAd() {
+
+  const button = document.querySelector("button");
+
+  button.innerText = "Watching Ad...";
+
+  button.disabled = true;
+
+  setTimeout(() => {
+
+    document
+      .getElementById("content")
+      .classList.remove("hidden");
+
+    button.innerText = "Unlocked 🔓";
+
+  }, 5000);
+
+}
 
   setTimeout(() => {
 
